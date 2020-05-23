9bcf25e64244b37d49a8028496e90363b5c069f7
Ash Hipgrave <AshHipgrave@users.noreply.github.com>
2020-05-23 19:09:10 +0100

    Updated shader compilation script / Recompiled modified shaders

---
+++ Shaders/Compile.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+
+:Compile
+ECHO Compiling shader source code...
+
+C:\VulkanSDK\1.2.135.0\Bin\glslc.exe shader.vert -o vert.spv
+C:\VulkanSDK\1.2.135.0\Bin\glslc.exe shader.frag -o frag.spv
+
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+GOTO Move
+
+:Move
+ECHO Copying compiled shaders...
+
+REM Required here when running under Visual Studio
+MOVE /Y vert.spv ../PlaygroundGameApp/vert.spv
+MOVE /Y frag.spv ../PlaygroundGameApp/frag.spv
+
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+GOTO Success
+
+:Error
+ECHO Build failed! See output for details.
+PAUSE
+EXIT
+
+:Success
+echo Success!
+EXIT
